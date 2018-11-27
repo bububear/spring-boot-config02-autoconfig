4fe4d5860adf291e4be4dd8bbdb1f10aa4ff0753
zuochong <847930998@qq.com>
2018-11-28 00:13:20 +0800

    first commit init project

---
+++ src/main/java/com/zuochong/springboot/SpringBootConfig02QutoconfigApplication.java
@@ -0,0 +1,12 @@
+package com.zuochong.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfig02QutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfig02QutoconfigApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zuochong/springboot/SpringBootConfig02QutoconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zuochong.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootConfig02QutoconfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
